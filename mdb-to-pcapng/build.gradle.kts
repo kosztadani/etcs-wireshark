5ea73d4113d8cb08ce4d47ef8e6ffe8628234af1
Dániel Koszta <daniel.koszta@gmail.com>
2025-05-26 08:29:13 +0200

    Add dependency for reading MDB files
    Change-Id: I9582aba97ab0736825fe7c459fe8d1fbb2fdfeea

--- mdb-to-pcapng/build.gradle.kts
+++ mdb-to-pcapng/build.gradle.kts
@@ -2,6 +2,19 @@ plugins {
     id("dev.kosztadani.etcs_wireshark.application")
 }
 
+// The "ucanaccess" library is an "automatic" module, but Gradle doesn't seem
+// to offer direct support for that.
+tasks.named<JavaCompile>("compileJava") {
+    options.compilerArgs.addAll(
+        listOf(
+            "--module-path", classpath.asPath
+        )
+    )
+}
+tasks.named<Javadoc>("javadoc") {
+    options.modulePath(classpath.files.toList())
+}
+
 val applicationMainClass = "dev.kosztadani.etcs_wireshark.mdb_to_pcapng.MdbToPcapngMain"
 
 application {
@@ -17,4 +30,5 @@ tasks.jar {
 }
 
 dependencies {
+    implementation(libs.ucanaccess)
 }
